553380e02650dabdc8f503f18fef021cf7273202
sachin11p12 <sachin11p12@gmail.com>
2024-12-22 22:51:05 +0530

    Third change

--- Demoo.java
+++ Demoo.java
@@ -2,5 +2,6 @@ public class Demoo {
     public static void main(String[] args){
         System.out.println(" Jay Shri Ram !");
         System.out.println("---------");
+        System.out.println(" Jai shri krishna !");
     }
 }
